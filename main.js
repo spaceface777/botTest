3db7cc171362117c3ad5a8925c4c5d8d64cba7e1
spaceface777 <spaceface7777@gmail.com>
2019-01-09 21:21:45 +0100

    remove useless console.log

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const HermesBot = require('hermesbot');
-const BOT_TOKEN = "913f2ed0-1125-11e9-9698-351fa8b8a04e"
+const BOT_TOKEN = "913f2ed0-1125-11e9-9698-351fa8b8a04e" // Get your own at https://hermesmessenger-testing.duckdns.org/createBot
 
 const bot = new HermesBot(BOT_TOKEN, {
     HermesURL: 'https://hermesmessenger-testing.duckdns.org',
@@ -9,6 +9,8 @@ const bot = new HermesBot(BOT_TOKEN, {
 bot.on('message', message => {
     console.log(message.sender + ': ' + message.text)
     console.log()
+
+    /*
     if (message.text.toLowerCase().indexOf('bot') != -1) {
         bot.quote(message, 'Thanks, ' + message.sender)
     }
@@ -17,6 +19,13 @@ bot.on('message', message => {
         bot.quote(message, 'Hi ' + message.sender)
         bot.sendMessage('general', 'How are you?')
     }
+*/
+    colorTest = message.text.split(' ') // '/setColor FF0000'
+    if (colorTest[0] == '/setColor') {
+        console.log('setting color to ' + colorTest[1])
+        bot.setColor(colorTest[1])
+    }
+
 });
 
 bot.on('quoted', message => {
